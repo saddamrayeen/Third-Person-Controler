80115162f64af0df68b640b3a4fc7f07b222412c
Saddam Rayeen <saddamrayeen010@gmail.com>
2023-05-07 09:53:01 +0530

    initial commit

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    GameObject playerBody;
+
+    void Start()
+    {
+        Rotate();
+    }
+
+    private void Rotate() { }
+}
